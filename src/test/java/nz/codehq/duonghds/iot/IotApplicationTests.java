282a8055487a7f4a46de9393d21561c153af1d58
duonghds <duong.hoang1@tiki.vn>
2021-12-16 18:37:40 +0700

    Initialize project

---
+++ src/test/java/nz/codehq/duonghds/iot/IotApplicationTests.java
@@ -0,0 +1,13 @@
+package nz.codehq.duonghds.iot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
