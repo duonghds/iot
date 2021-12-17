c0e2be29e512dd6e496e4c05618a9107559e0e39
duonghds <duong.hoang1@tiki.vn>
2021-12-17 11:58:28 +0700

    deploy heroku

---
+++ src/main/java/nz/codehq/duonghds/iot/config/DatabaseConfig.java
@@ -0,0 +1,20 @@
+package nz.codehq.duonghds.iot.config;
+
+import com.zaxxer.hikari.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.*;
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        return new HikariDataSource(config);
+    }
+}
