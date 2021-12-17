c039ae97889d75373a39730a216650c1e3876db3
duonghds <duong.hoang1@tiki.vn>
2021-12-17 12:49:35 +0700

    feat: remove unused import

--- src/main/java/nz/codehq/duonghds/iot/controller/DataDeviceController.java
+++ src/main/java/nz/codehq/duonghds/iot/controller/DataDeviceController.java
@@ -50,8 +50,6 @@ public class DataDeviceController {
             @PathVariable(value = "deviceId") String deviceId,
             @RequestParam(name = "start", required = false ) Long start,
             @RequestParam(name = "end", required = false ) Long end) {
-        System.out.println("start = " + start);
-        System.out.println("end = " + end);
         ListDataDeviceDto listDataDevice = dataDeviceService.findByDeviceIdAndTime(deviceId, start, end);
         if(listDataDevice == null) {
             return ResponseEntity.notFound().build();
--- src/main/java/nz/codehq/duonghds/iot/service/DataDeviceService.java
+++ src/main/java/nz/codehq/duonghds/iot/service/DataDeviceService.java
@@ -3,9 +3,6 @@ package nz.codehq.duonghds.iot.service;
 
 import nz.codehq.duonghds.iot.dto.DataDeviceDto;
 import nz.codehq.duonghds.iot.dto.ListDataDeviceDto;
-
-import java.util.Date;
-
 public interface DataDeviceService {
     void save(DataDeviceDto dataDeviceDto);
     ListDataDeviceDto findByDeviceIdAndTime(String deviceId, Long start, Long end);
